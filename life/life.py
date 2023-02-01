66eca060584f01b0e29ed79f832df8d28410bdd0
maxclarke1 <mc621@ic.ac.uk>
2023-02-01 13:15:52 +0000

    Correct formatting and docstrings

--- life/life.py
+++ life/life.py
@@ -2,12 +2,12 @@ import numpy as np
 from matplotlib import pyplot
 from scipy.signal import convolve2d
 
-glider = np.array([[0,1,0],[0,0,1],[1,1,1]])
+glider = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
 
 blinker = np.array([
-[0,0,0],
-[1,1,1],
-[0,0,0]])
+ [0, 0, 0],
+ [1, 1, 1],
+ [0, 0, 0]])
 
 glider_gun = np.array([
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -48,30 +48,63 @@ glider_gun = np.array([
     [0, 0, 1, 1, 0, 0, 0, 0, 0]
 ])
 
+
 class Game:
-    def __init__(game,Size):
+    """
+    A class used to represent the game of life.
+
+    Attributes
+    ----------
+    board: a Size x Size board for the game.
+    """
+
+    def __init__(game, Size):
+        """
+        Parameters
+        ----------
+        Size : int
+            The dimension of the board in the game of life.
+        """
+
         game.board = np.zeros((Size, Size))
-    
+
     def play(self):
+        """
+        Starts the game and keeps it running.
+        """
+
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
-            self.move(); self.show()
+            self.move(), self.show()
             pyplot.pause(0.0000005)
 
     def move(self):
+        """
+        Decides if a cell live or dies.
+        """
+
         STENCIL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
         NeighbourCount = convolve2d(self.board, STENCIL, mode='same')
 
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
-                self.board[i,j]=1 if (NeighbourCount[i, j]==3 or (NeighbourCount[i, j]==2 and self.board[i,j])) else 0
+                self.board[i, j] = 1 if (NeighbourCount[i, j] == 3 or
+                                         (NeighbourCount[i, j] == 2 and
+                                         self.board[i, j])) else 0
 
-    def __setitem__(self, key, value): 
+    def __setitem__(self, key, value):
+        """
+        Sets specific value to square on board.
+        """
+
         self.board[key] = value
 
-
     def show(self):
-        pyplot.clf() 
+        """
+        Outputs the moves from the game of life.
+        """
+
+        pyplot.clf()
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
